5e221d005a050f95e786cbfe5918514698803f9b
FlaGit1 <flaviano.oliveira@globalhitss.com.br>
2024-08-22 01:48:36 -0300

    Operadores Java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+      boolean condicao1 = true;
+
+      boolean condicao2 = true;
+
+      if(condicao1 && ( 7 > 4 ) ){
+        System.out.println("As duas condições são VERDADEIRAS");
+      }
+
+      if(condicao1 || condicao2){
+        System.out.println("Uma das condições é VERDADEIRA");
+      }
+
+      System.out.println("fim");
+
+    }
+    
+}
