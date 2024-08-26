43c2cddf56c4bc39ae05350cebfd8b12563b2b6b
FlaGit1 <flaviano.oliveira@globalhitss.com.br>
2024-08-26 01:58:24 -0300

    Projeto Conta Banco

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Agora digite seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Exibindo a mensagem final ao cliente:
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " +
+                agencia + ", conta " + numeroConta + " e seu saldo R$ " + saldo + " já está disponível para saque.");
     }
 }
