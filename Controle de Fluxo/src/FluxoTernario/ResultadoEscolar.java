5ae93c8fbdf7362c969daeab67b0f5623335eaf5
FlaGit1 <flaviano.oliveira@globalhitss.com.br>
2024-08-29 02:00:35 -0300

    Fluxos

---
+++ Controle de Fluxo/src/FluxoTernario/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+package FluxoTernario;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 5;
+
+        String resultado = nota >= 7 ? "APROVADO" : "REPROVADO";
+
+        System.out.println(resultado);
+    }
+    
+}
---
+++ Controle de Fluxo/src/FluxoTernario/ResultadoEscolar2.java
@@ -0,0 +1,12 @@
+package FluxoTernario;
+
+public class ResultadoEscolar2 {
+    public static void main(String[] args) {
+        int nota = 4;
+
+        String resultado = nota >= 7 ? "APROVADO" : nota >= 5 && nota < 7 ? "RECUPERAÇÃO" : "REPROVADO";
+
+        System.out.println(resultado);
+    }
+    
+}
