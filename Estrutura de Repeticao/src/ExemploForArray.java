74da0c1192957abfe8727323ad55db97dc264285
FlaGit1 <flaviano.oliveira@globalhitss.com.br>
2024-08-30 00:23:17 -0300

    Exemplos

---
+++ Estrutura de Repeticao/src/ExemploForArray.java
@@ -0,0 +1,13 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+            //Em Arrays o índice inicia em ZERO
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+    for (int x = 0; x < alunos.length; x++) {
+        
+        System.out.println("O aluno no índice x=" + x + " é " + alunos [ x ] );
+    }
+    }
+    
+}
