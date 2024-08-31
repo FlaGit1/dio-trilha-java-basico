4fce90d4006c45e60caa697ed76a2af4c5081768
FlaGit1 <flaviano.oliveira@globalhitss.com.br>
2024-08-31 00:27:27 -0300

    Exceções

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,8 +1,10 @@
 import java.util.Locale;
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 public class AboutMe {
     public static void main(String[] args) {
+
+        try{
         //Criando o Objeto Scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
@@ -20,9 +22,14 @@ public class AboutMe {
 
 
         //imprimindo os dados obtidos pelo o usuário
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é: " + altura + "cm ");
+        scanner.close();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser numéricos");
+        }
     }
     
 }
